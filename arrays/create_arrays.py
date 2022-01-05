b6714af4886a0a3c57dc4957f5f4301a11d22007
alex <arodriguez202020@gmail.com>
2022-01-05 12:56:43 -0600

    add lambda and arrays example folders

---
+++ arrays/create_arrays.py
@@ -0,0 +1,32 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+print(cars)
+
+# access array index value
+x = cars[0]
+
+print(x)
+
+# change value of array index
+cars[0] = "Toyota"
+
+print(cars)
+
+# array length
+x = len(cars)
+
+print(x)
+
+# loop through array
+for x in cars:
+    print(x)
+
+# add element to array
+cars.append("Honda")
+
+print(cars)
+
+# remove an element from array
+cars.pop(1)
+
+print(cars)
---
+++ lambda/lambda_func.py
@@ -0,0 +1,19 @@
+def x(a): return a + 10
+
+
+print(x(5))
+
+
+# lamda function with arguments
+def x(a, b): return a * b
+
+
+print(x(5, 6))
+
+# lambda sum function
+
+
+def x(a, b, c): return a + b + c
+
+
+print(x(5, 6, 2))
